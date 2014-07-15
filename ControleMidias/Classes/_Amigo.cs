c1d115d8b3972d793184014ba2fb6bf17779cc27
RobertoOll <robertooliveiraestudo@gmail.com>
2014-07-15 15:09:43 -0300

    First Commit
    Commit do projeto inacabado... Parte do banco de dados pronta...

---
+++ ControleMidias/Classes/_Amigo.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControleMidias.BancoDeDados;
+
+namespace ControleMidias.Classes
+{
+    class _Amigo
+    {
+        /// <summary>
+        /// Esta classe representa a entidade amigo, fornecendo todas as operações necessárias
+        /// para a manutenção dos dados da tabela 'Amigo' no banco de dados 'BancoControleMidia'. 
+        /// </summary>
+        
+        // Atributos globais encapsulados
+        private uint idAmigo;
+        private string nome;
+        private string apelido;
+        private string celular;
+        private string telefone;
+        private string email;
+        private string cidade;
+        private string bairro;
+        private string numeroCasa;
+        private string nomePai;
+        private string nomeMae;
+
+        // Propriedades
+        public uint IdAmigo
+        {
+            get { return idAmigo; }
+            set { idAmigo = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Apelido
+        {
+            get { return apelido; }
+            set { apelido = value; }
+        }
+
+        public string Celular
+        {
+            get { return celular; }
+            set { celular = value; }
+        }
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Cidade
+        {
+            get { return cidade; }
+            set { cidade = value; }
+        }
+
+        public string Bairro
+        {
+            get { return bairro; }
+            set { bairro = value; }
+        }
+
+        public string NumeroCasa
+        {
+            get { return numeroCasa; }
+            set { numeroCasa = value; }
+        }
+
+        public string NomePai
+        {
+            get { return nomePai; }
+            set { nomePai = value; }
+        }
+
+        public string NomeMae
+        {
+            get { return nomeMae; }
+            set { nomeMae = value; }
+        }
+
+        // Método para salvar os dados
+        public bool SalvarDados()
+        {
+            try
+            {
+                LinqControleMidiaDataContext dc = new LinqControleMidiaDataContext();
+
+                Amigo amigo = new Amigo();
+                amigo.Nome = nome;
+                amigo.Apelido = apelido;
+                amigo.Celular = celular;
+                amigo.Telefone = telefone;
+                amigo.Email = email;
+                amigo.Cidade = cidade;
+                amigo.Bairro = bairro;
+                amigo.NumeroCasa = numeroCasa;
+                amigo.NomePai = nomePai;
+                amigo.NomeMae = nomeMae;
+                dc.Amigos.InsertOnSubmit(amigo);
+                dc.SubmitChanges();
+
+                return true; // Retorna true caso a operação ocorra com sucesso
+            }
+            catch (Exception)
+            {
+                // Para obter detalhes da excessão (se ocorrer), pode-se tornar a linha abaixo um comentário
+                return false; // Retorna false caso ocorra uma excessão na operação
+                throw;
+            }
+        }
+
+        // Método para alterar os dados
+        public bool AlterarDados(uint id)
+        {
+            try
+            {
+                LinqControleMidiaDataContext dc = new LinqControleMidiaDataContext();
+
+                var Pesquisa = from Amigo in dc.Amigos
+                               where Amigo.IdAmigo == id
+                               select Amigo;
+
+                Amigo amigo = new Amigo();
+                amigo = Pesquisa.Single();
+                amigo.Nome = nome;
+                amigo.Apelido = apelido;
+                amigo.Celular = celular;
+                amigo.Telefone = telefone;
+                amigo.Email = email;
+                amigo.Cidade = cidade;
+                amigo.Bairro = bairro;
+                amigo.NumeroCasa = numeroCasa;
+                amigo.NomePai = nomePai;
+                amigo.NomeMae = nomeMae;
+                dc.SubmitChanges();
+
+                return true; // Retorna true caso a operação ocorra com sucesso
+            }
+            catch (Exception)
+            {
+                // Para obter detalhes da excessão (se ocorrer), pode-se tornar a linha abaixo um comentário
+                return false; // Retorna false caso ocorra uma excessão na operação
+                throw;
+            }
+        }
+
+        // Método para excluir os dados
+        public bool ExcluirDados(uint id)
+        {
+            try
+            {
+                LinqControleMidiaDataContext dc = new LinqControleMidiaDataContext();
+
+                var Pesquisa = from Amigo in dc.Amigos
+                               where Amigo.IdAmigo == id
+                               select Amigo;
+
+                Amigo amigo = new Amigo();
+                amigo = Pesquisa.Single();
+                dc.Amigos.DeleteOnSubmit(amigo);
+                dc.SubmitChanges();
+                return true; // Retorna true caso a operação ocorra com sucesso
+            }
+            catch (Exception)
+            {
+                // Para obter detalhes da excessão (se ocorrer), pode-se tornar a linha abaixo um comentário
+                return false; // Retorna false caso ocorra uma excessão na operação
+                throw;
+            }
+        }
+    }
+}
---
+++ ControleMidias/Classes/_Midia.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControleMidias.BancoDeDados;
+
+namespace ControleMidias.Classes
+{
+    class _Midia
+    {
+        /// <summary>
+        /// Esta classe representa a entidade amigo, fornecendo todas as operações necessárias
+        /// para a manutenção dos dados da tabela 'Amigo' no banco de dados 'BancoControleMidia'. 
+        /// </summary>
+
+        // Atributos globais encapsulados
+        //  Da mídia:
+        private uint idMidia;
+        private string nome;
+        private string tipo;
+        //  Do empréstimo:
+        private uint idAmigo;
+        private DateTime dataSaida;
+        private DateTime dataPrevistaEntrega;
+        private DateTime dataEfetivaEntrega;
+
+        // Propriedades
+        
+        // Método para salvar os dados
+        public bool AlterarDados()
+        {
+            try
+            {
+                LinqControleMidiaDataContext dc = new LinqControleMidiaDataContext();
+                return true; // Retorna true caso a operação ocorra com sucesso
+            }
+            catch (Exception)
+            {
+                // Para obter detalhes da excessão (se ocorrer), pode-se tornar a linha abaixo um comentário
+                return false; // Retorna false caso ocorra uma excessão na operação
+                throw;
+            }
+        }
+
+        // Método para alterar os dados
+        public bool AlterarDados(uint id)
+        {
+            try
+            {
+                LinqControleMidiaDataContext dc = new LinqControleMidiaDataContext();
+                return true; // Retorna true caso a operação ocorra com sucesso
+            }
+            catch (Exception)
+            {
+                // Para obter detalhes da excessão (se ocorrer), pode-se tornar a linha abaixo um comentário
+                return false; // Retorna false caso ocorra uma excessão na operação
+                throw;
+            }
+        }
+
+        // Método para excluir os dados
+        public bool AlterarDados(uint id)
+        {
+            try
+            {
+                LinqControleMidiaDataContext dc = new LinqControleMidiaDataContext();
+                return true; // Retorna true caso a operação ocorra com sucesso
+            }
+            catch (Exception)
+            {
+                // Para obter detalhes da excessão (se ocorrer), pode-se tornar a linha abaixo um comentário
+                return false; // Retorna false caso ocorra uma excessão na operação
+                throw;
+            }
+        }
+    }
+}
